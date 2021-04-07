9e5e70e0789a6fe8055760148d506e1eb1c114df
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2021-04-06 23:22:58 -0400

    add daxpy microbenchmark

---
+++ microbenchmarks/README.md
@@ -0,0 +1,1 @@
+Microbenchmarks to verify the correctness and speed of a few small examples of cilk for range loops.
---
+++ microbenchmarks/daxpy.cpp
@@ -0,0 +1,56 @@
+using namespace std;
+#include <vector>
+#include <cilk/cilk.h>
+#include <iostream>
+#include "fasttime.h"
+
+void daxpy_forrange(vector<pair<double, double>> yx, double a) {
+    cilk_for (auto &yxi : yx) {
+        yxi.first = a * yxi.second;
+    }
+}
+void daxpy_for(vector<pair<double, double>> yx, double a) {
+    cilk_for (int i = 0; i < yx.size(); i++) {
+        yx[i].first = a * yx[i].second;
+    }
+}
+
+void daxpy_test(void (*f)(vector<pair<double, double>>, double), string desc) {
+    cout << "RUNNING TEST: " << desc << endl;
+
+    int N = 64;
+    vector<pair<double, double>> v(N);
+    for (int i = 0; i < N; i++) {
+        v[i] = make_pair(double(rand()), double(rand()));
+    }
+
+    double a = double(rand());
+
+    vector<pair<double, double>> v2(v);
+    fasttime_t time1 = gettime();
+    f(v2, a);
+    fasttime_t time2 = gettime();
+    
+    // verify result
+    f(v, a);
+    for (int i = 0; i < N; i++) {
+        assert(v[i] == v2[i]);
+    }
+
+    // print time
+    double elapsed = tdiff(time1, time2);
+    printf("Elapsed execution time: %f sec\n", elapsed);
+}
+
+int main()
+{
+    srand(time(0));
+
+#ifdef FORRANGE
+    daxpy_test(daxpy_forrange, "daxpy_forrange");
+#else
+    daxpy_test(daxpy_for, "daxpy_for");
+#endif
+
+    return 0;
+}
